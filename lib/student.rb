b6eaa1711e167b322af71a5dfaf4b2697ed42a24
Eric Fiorani <efiorani305@gmail.com>
2018-07-10 19:30:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
 
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS student (
+    CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade TEXT
@@ -26,7 +26,7 @@ class Student
   end
 
   def self.drop_table
-    sql = "DROP TABLE IF EXISTS student"
+    sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
 
@@ -35,7 +35,7 @@ class Student
       self.update
     else
       sql = <<-SQL
-        INSERT INTO student (name, grade)
+        INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
@@ -44,7 +44,7 @@ class Student
   end
 
   def self.create(name:, grade:)
-    students = Students.new(name, grade)
+    students = Student.new(name, grade)
     students.save
     students
   end
